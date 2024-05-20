6d1e686c064ffb7fbedb71f4b0a2105382f11f43
GeovannaCosta <158053742+GeovannaCosta@users.noreply.github.com>
2024-05-20 15:56:01 -0300

    Adicionar arquivos de projeto.

---
+++ lista 3.5/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace lista{
+
+    class Program{
+      static void Main(string[] args)
+        {
+            /*Console.Write("Informe o login:");
+            string login = Console.ReadLine();
+            Console.Write("Informe a senha:");
+            string senha = Console.ReadLine();
+
+            if (login == "admin" && senha == "1234")
+            {
+                Console.WriteLine("Pode acessar");
+            }
+            else
+            {
+                Console.WriteLine("Login ou senha incorreto");
+            }*/
+
+
+            /*Console.WriteLine("Informe a quantidade de blusas no estoque:");
+            int quantidade = int.Parse(Console.ReadLine());
+            
+            if ( quantidade > 40) {
+                Console.WriteLine("ESTOQUE OK");
+                    }
+            else if ( quantidade > 20 && quantidade <= 40)
+            {
+                Console.WriteLine("ATEÇÃO AO ESTOQUE");
+            }
+            else
+            {
+                Console.WriteLine("ESTOQUE CRITICO");
+            }*/
+
+
+            /*Console.WriteLine("Informe sua idade:");
+            int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe seu salário");
+            decimal salario = decimal.Parse(Console.ReadLine());
+
+            if (idade >= 18 && salario > 2000) 
+            {
+                Console.WriteLine("PODE VIAJAR");
+            }
+            else
+            {
+                Console.WriteLine("NÃO PODE VIAJAR");
+            }*/
+
+
+            /*Console.WriteLine("Escolha uma aula digitando um número (1-4):");
+            int escolha = int.Parse(Console.ReadLine());
+
+            if (escolha == 1 || escolha == 2 ) 
+            {
+                Console.WriteLine("DESENHO E PINTURA");
+            }
+             else if (escolha == 3 || escolha == 4)
+            {
+                Console.WriteLine("DISCURSO E DEBATE");
+            }
+            else
+            {
+                Console.WriteLine("DANÇA");
+            }*/
+
+
+            /*Console.WriteLine("Digite um número:");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero > 0 && numero > 100)
+            {
+                Console.WriteLine("número especial");
+            }
+            else
+            {
+                Console.WriteLine("fora do intervalo");
+            }*/
+        }
+
+    }
+}
